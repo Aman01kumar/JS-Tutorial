28421a52b75c2de372494bb6cea96ac52d30e488
Aman01kumar <sammar4568@gmail.com>
2024-11-14 00:04:41 +0530

    Arrays configure

---
+++ 02_start/01_Arrays.js
@@ -0,0 +1,41 @@
+//array
+
+const myArray = [0,1,2,3,4,5]
+const myHeroes = ["ironman", "doom"]
+const myArray2 = new Array(1,2,3,4)
+//console.log(myArray[1]);
+
+// Arrays methods
+//myArray.push(6)
+//myArray.push(7)
+//console.log(myArray);
+//myArray.pop()
+
+//myArray.unshift(7)
+//myArray.shift()
+
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(14));
+
+//const newArr = myArray.join()
+
+//console.log(myArray);
+//console.log(newArr);
+//console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArray);
+
+const myn1 = myArray.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArray);
+
+const myn2 = myArray.splice(1, 3)
+console.log("c ", myArray);
+console.log(myn2);
+
+
+
+
+
