d7319d9e4ab6dd39edbd44cb20886e4091fff79c
Aman01kumar <sammar4568@gmail.com>
2024-11-18 18:36:15 +0000

    changes made and updated

--- 03_start/01_functions.js
+++ 03_start/01_functions.js
@@ -48,7 +48,44 @@ function loginUserMessage(username="ass"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Aman"));
+//console.log(loginUserMessage("Aman"));
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+
+}
+
+//console.log(calculateCartPrice(200, 400,600,700, 800));
+
+const user = {
+    username: "Aman",
+    price: 1999
+}
+
+function handleObject(anyObject){
+
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+
+}
+
+//handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 200
+})
+
+const newArray = [200, 400, 600, 800]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([200, 400, 600, 800]));
+
+
+
 
 
 
